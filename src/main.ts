c636103f081bc4b46664ca15457466913d83d597
balogun akeem <balogunbiola101@gmail.com>
2023-10-02 11:45:10 +0100

    fix stuff

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ import { AppModule } from './app.module';
 
 const express = require('express');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule)
   app.enableCors({
     origin: [
       'http://localhost:3000',
