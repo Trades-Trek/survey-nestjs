adc2f1acf1cf4ae283a23220d16823897125f3cf
balogun akeem <balogunbiola101@gmail.com>
2023-10-02 14:00:00 +0100

    fix for render

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,25 @@ import { AppModule } from './app.module';
 
 const express = require('express');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://nats-survey.vercel.app',
-      'https://nats-survey.vercel.app/',
-    ],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-      preflightContinue: true,
-      optionsSuccessStatus: 200
-  });
+  const app = await NestFactory.create(AppModule,{
+
+    cors:true
+    
+    })
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://localhost:3001',
+  //     'https://nats-survey.vercel.app',
+  //     'https://nats-survey.vercel.app/',
+  //   ],
+  //   methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+  //   credentials: true,
+  //   allowedHeaders:
+  //     'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  //     preflightContinue: true,
+  //     optionsSuccessStatus: 200
+  // });
   app.use('/uploads', express.static('uploads'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(ENV.PORT);
