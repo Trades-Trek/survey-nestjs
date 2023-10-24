cbbc01e5d73676ff936af0ec7b6e398b6caf8384
balogun akeem <balogunbiola101@gmail.com>
2023-10-24 20:12:44 +0100

    deploy latest changes

--- config/environment.ts
+++ config/environment.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 const DEV = {
-  // DB_URL: 'mongodb://localhost:27017/netsurveys',
-  DB_URL: 'mongodb+srv://jkdjkbsjdbcuwbchkjdcwd:fdcZExD1wnVcv7bV@cluster0.elpsihy.mongodb.net/netsurveys?retryWrites=true&w=majority',
+  DB_URI: 'mongodb+srv://jkdjkbsjdbcuwbchkjdcwd:fdcZExD1wnVcv7bV@cluster0.elpsihy.mongodb.net/netsurveys?retryWrites=true&w=majority',
    DB_USER: '',
   DB_PASS: '',
   JWT_SECRET_KEY: 'thisissecretkey',
